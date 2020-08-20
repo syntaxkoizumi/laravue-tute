69eae1c7259b3c1306b69bd6bc0556c83048f8cd
ShintaroKoizumi <chjt26@gmail.com>
2020-08-21 00:47:15 +0900

    feat: setting laravel mix webpack

--- web/webpack.mix.js
+++ web/webpack.mix.js
@@ -12,4 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .version()
+
+mix.browserSync({
+    proxy: '0.0.0.0:8081',
+    open: false
+})
